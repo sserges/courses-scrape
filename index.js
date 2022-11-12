28baf1141dfa4efb106d502e5b73cfee3b2ed3d7
sserges <agounkeserge@gmail.com>
2022-11-12 18:12:07 -0400

    Init Browser & Page Object

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(123)
+const puppeteer = require('puppeteer')
+
+async function run() {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.traversymedia.com')
+
+  await browser.close()
+}
+
+run()
