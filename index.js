5b121607314c4496c2463176775eee622d3d3f9c
sserges <agounkeserge@gmail.com>
2022-11-12 18:21:00 -0400

    Screenshot & PDF

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ async function run() {
   const page = await browser.newPage()
   await page.goto('https://www.traversymedia.com')
 
+  //   await page.screenshot({ path: 'example1.png', fullPage: true })
+  await page.pdf({ path: 'example1.pdf', format: 'A4' })
+
   await browser.close()
 }
 
