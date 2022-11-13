634121e89b5cb355c23a97b95a668a2202c9182e
sserges <agounkeserge@gmail.com>
2022-11-13 16:46:08 -0400

    Targeting HTML, Text, and Links

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ async function run() {
   await page.goto('https://www.traversymedia.com')
 
   //   await page.screenshot({ path: 'example1.png', fullPage: true })
-  await page.pdf({ path: 'example1.pdf', format: 'A4' })
+  //   await page.pdf({ path: 'example1.pdf', format: 'A4' })
+  //   const html = await page.content()
+
+  //   const title = await page.evaluate(() => document.title)
+
+  // const text = await page.evaluate(() => document.body.innerText)
+
+  const links = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('a'), (e) => e.href)
+  )
+  console.log(links)
 
   await browser.close()
 }
